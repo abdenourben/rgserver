3f597623a1b8224842e75d8b75471b18f47127b4
AhlemOuldAli <32915388+AhlemOuldAli@users.noreply.github.com>
2019-08-24 19:46:16 +0200

    Update DemandePermis.java

--- src/main/java/dz/rgserver/model/DemandePermis.java
+++ src/main/java/dz/rgserver/model/DemandePermis.java
@@ -1,55 +1,74 @@
 package dz.rgserver.model;
 
 import java.io.Serializable;
+import java.util.List;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="demandesPermis")
 public class DemandePermis implements Serializable{
 
-	private static final long serialVersionUID = -6483665147880708156L;
+private static final long serialVersionUID = -6483665147880708156L;
 
-	@Id @GeneratedValue(strategy = GenerationType.AUTO)
-	private long id_demande;
-	private String etat; //de la demande, etapes du workflow
-	public DemandePermis(String etat, String categorie) {
-		super();
-		this.etat = etat;
-		this.categorie = categorie;
-	}
+@Id @GeneratedValue(strategy = GenerationType.AUTO)
+@Column(name="id_demande")
+private long id_demande;
+private String etat; //de la demande, etapes du workflow
+public DemandePermis(String etat, String categorie) {
+super();
+this.etat = etat;
+this.categorie = categorie;
+}
+//relation demande _user
+@OneToMany(mappedBy="demande",cascade=CascadeType.ALL,fetch=FetchType.EAGER)
+@JoinColumn(name="id_demande",referencedColumnName="user-id")
+private List<User> users;
 
-	public long getId_demande() {
-		return id_demande;
-	}
+public List<User> getUsers() {
+return users;
+}
 
-	public void setId_demande(long id_demande) {
-		this.id_demande = id_demande;
-	}
+public void setUsers(List<User> users) {
+this.users = users;
+}
 
-	public String getEtat() {
-		return etat;
-	}
+public long getId_demande() {
+return id_demande;
+}
 
-	public void setEtat(String etat) {
-		this.etat = etat;
-	}
+public void setId_demande(long id_demande) {
+this.id_demande = id_demande;
+}
 
-	public String getCategorie() {
-		return categorie;
-	}
+public String getEtat() {
+return etat;
+}
 
-	public void setCategorie(String categorie) {
-		this.categorie = categorie;
-	}
+public void setEtat(String etat) {
+this.etat = etat;
+}
 
-	private String categorie; //de la demande
+public String getCategorie() {
+return categorie;
+}
 
-	public DemandePermis() {
-	}
+public void setCategorie(String categorie) {
+this.categorie = categorie;
+}
+
+private String categorie; //de la demande
+
+public DemandePermis() {
+}
 
 }
