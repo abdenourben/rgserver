a6b3a5a7fb2eec34522596b24ded6419172e975d
AhlemOuldAli <32915388+AhlemOuldAli@users.noreply.github.com>
2019-08-24 19:47:43 +0200

    Update Image.java

--- src/main/java/dz/rgserver/model/Image.java
+++ src/main/java/dz/rgserver/model/Image.java
@@ -2,10 +2,15 @@ package dz.rgserver.model;
 
 import java.io.Serializable;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -13,68 +18,83 @@ import javax.persistence.Table;
 
 public class Image implements Serializable {
 
-	private static final long serialVersionUID = 5836911806971607169L;
+private static final long serialVersionUID = 5836911806971607169L;
 
-	@Id @GeneratedValue(strategy = GenerationType.AUTO)
-	private long id_image;
-	private String titre;
-	private String description;
-	private String chemin;
-	private String type;
+@Id @GeneratedValue(strategy = GenerationType.AUTO)
+@Column(name="id_image")
+private long id_image;
+private String titre;
+private String description;
+private String chemin;
+private String type;
 
-	public Image() {
-	}
+//relation image-RG
+@OneToOne(mappedBy="rimage",cascade=CascadeType.ALL,fetch=FetchType.EAGER)
+@JoinColumn(name="id_image",referencedColumnName="id_ressource")
+private RessourceGenetique ressourceGenetiques;
 
-	public Image(String titre, String description, String chemin, String type) {
-		super();
-		this.titre = titre;
-		this.description = description;
-		this.chemin = chemin;
-		this.type = type;
-	}
 
-	public long getId_image() {
-		return id_image;
-	}
+public RessourceGenetique getRessourceGenetiques() {
+return ressourceGenetiques;
+}
 
-	public void setId_image(long id_image) {
-		this.id_image = id_image;
-	}
+public void setRessourceGenetiques(RessourceGenetique ressourceGenetiques) {
+this.ressourceGenetiques = ressourceGenetiques;
+}
 
-	public String getTitre() {
-		return titre;
-	}
+public Image() {
+}
 
-	public void setTitre(String titre) {
-		this.titre = titre;
-	}
+public Image(String titre, String description, String chemin, String type) {
+super();
+this.titre = titre;
+this.description = description;
+this.chemin = chemin;
+this.type = type;
+}
 
-	public String getDescription() {
-		return description;
-	}
+public long getId_image() {
+return id_image;
+}
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+public void setId_image(long id_image) {
+this.id_image = id_image;
+}
 
-	public String getChemin() {
-		return chemin;
-	}
+public String getTitre() {
+return titre;
+}
 
-	public void setChemin(String chemin) {
-		this.chemin = chemin;
-	}
+public void setTitre(String titre) {
+this.titre = titre;
+}
 
-	public String getType() {
-		return type;
-	}
+public String getDescription() {
+return description;
+}
 
-	public void setType(String type) {
-		this.type = type;
-	}
+public void setDescription(String description) {
+this.description = description;
+}
 
-	public void setid_image(long id) {
-		this.id_image = id;
-	}
+public String getChemin() {
+return chemin;
+}
+
+public void setChemin(String chemin) {
+this.chemin = chemin;
+}
+
+public String getType() {
+return type;
+}
+
+public void setType(String type) {
+this.type = type;
+}
+
+public void setid_image(long id) {
+this.id_image = id;
+}
 
 }
