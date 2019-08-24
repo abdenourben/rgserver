699b9672eb2dffc41653611832905302076dd1be
AhlemOuldAli <32915388+AhlemOuldAli@users.noreply.github.com>
2019-08-24 20:11:03 +0200

    Update RegionRepository.java

--- src/main/java/dz/rgserver/dao/RegionRepository.java
+++ src/main/java/dz/rgserver/dao/RegionRepository.java
@@ -1,6 +1,8 @@
 package dz.rgserver.dao;
 
 import dz.rgserver.model.Region;
+import dz.rgserver.model.RessourceGenetique;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,4 +14,6 @@ public interface RegionRepository  extends JpaRepository<Region, Long> {
 	@Query(" select region from Region as region where region.designation like :x")
 	public Page<Region> chercherRegions (@Param("x")String mc , Pageable pageable );
 	
+	@Query(" select ressourceGenetiques from Region as ressources where region.wilaya like :wilaya")
+	public Page<RessourceGenetique> chercherWilaya_RG (@Param("wilaya")String wilaya, Pageable pageable);
 }
