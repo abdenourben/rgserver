a6b2f2162a3e19373ef4816a1396414982932671
AhlemOuldAli <32915388+AhlemOuldAli@users.noreply.github.com>
2019-08-24 20:06:03 +0200

    Update ClassificationRepository.java

--- src/main/java/dz/rgserver/dao/ClassificationRepository.java
+++ src/main/java/dz/rgserver/dao/ClassificationRepository.java
@@ -1,6 +1,8 @@
 package dz.rgserver.dao;
 
 import dz.rgserver.model.Classification;
+import dz.rgserver.model.RessourceGenetique;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,4 +14,6 @@ public interface ClassificationRepository extends JpaRepository<Classification, 
 	@Query(" select classe from Classification as classe where classe.designation like :x")
 	public Page<Classification> chercherClassifications (@Param("x")String mc , Pageable pageable );
 	
+	@Query(" select ressourceGenetiques from Classification as ressources where Classification.designation like :designation")
+	public Page<RessourceGenetique> chercherClassification_RG(@Param("designation")String designation_classification, Pageable pageable);
 }
