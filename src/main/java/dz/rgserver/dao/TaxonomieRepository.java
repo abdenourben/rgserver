b1584f86380e2bcba809a60810b39e66865f6230
AhlemOuldAli <32915388+AhlemOuldAli@users.noreply.github.com>
2019-08-24 20:17:23 +0200

    Update TaxonomieRepository.java

--- src/main/java/dz/rgserver/dao/TaxonomieRepository.java
+++ src/main/java/dz/rgserver/dao/TaxonomieRepository.java
@@ -1,5 +1,6 @@
 package dz.rgserver.dao;
 
+import dz.rgserver.model.RessourceGenetique;
 import dz.rgserver.model.Taxonomie;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -12,4 +13,20 @@ public interface TaxonomieRepository extends JpaRepository<Taxonomie, Long> {
 	@Query(" select taxonomie from Taxonomie as taxonomie where taxonomie.classe like :x")
 	public Page<Taxonomie> chercherTaxonomies (@Param("x")String mc , Pageable pageable );
 	
+	@Query(" select ressourceGenetiques from Taxonomie as ressources where Taxonomie.classe like :taxonomieClasse")
+	public Page<RessourceGenetique> chercherTaxonomieClasse_RG (@Param("taxonomieClasse")String taxonomieClasse , Pageable pageable );
+
+	@Query(" select ressourceGenetiques from Taxonomie as ressources where Taxonomie.ordre like :taxonomieOrdre")
+	public Page<RessourceGenetique> chercherTaxonomieOrdre_RG (@Param("taxonomieOrdre")String taxonomieOrdre , Pageable pageable );
+
+	@Query(" select ressourceGenetiques from Taxonomie as ressources where Taxonomie.famille like :taxonomieFamille")
+	public Page<RessourceGenetique> chercherTaxonomieFamille_RG (@Param("taxonomieFamille")String taxonomieFamille , Pageable pageable );
+
+	@Query(" select ressourceGenetiques from Taxonomie as ressources where Taxonomie.genre like :taxonomieGenre")
+	public Page<RessourceGenetique> chercherTaxonomieGenre_RG (@Param("taxonomieGenre")String taxonomieGenre , Pageable pageable );
+
+	@Query(" select ressourceGenetiques from Taxonomie as ressources where Taxonomie.espece like :taxonomieEspece")
+	public Page<RessourceGenetique> chercherTaxonomieEspece_RG (@Param("taxonomieEspece")String taxonomieEspece , Pageable pageable );
+
+
 }
